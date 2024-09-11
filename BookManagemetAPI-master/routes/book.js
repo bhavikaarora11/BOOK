1ad5bbf8ba95d7d71a681000f9e54232421b6f92
bhavikaarora11 <122777684+bhavikaarora11@users.noreply.github.com>
2024-09-11 15:29:05 +0530

    Update book.js

--- BookManagemetAPI-master/routes/book.js
+++ BookManagemetAPI-master/routes/book.js
@@ -74,20 +74,6 @@ router.get('/search', async (req, res) => {
 });
 
 
-// GET /books/:id: Retrieve the details of a single book by its ID
-router.get('/:id', async (req, res) => {
-    try {
-        const book = await Book.findById(req.params.id);
-
-        if (!book) {
-            return res.status(404).json({ message: 'Book not found' });
-        }
-
-        res.status(200).json(book);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
 
 // PUT /books/:id: Update an existing book's details
 router.put('/:id', async (req, res) => {
